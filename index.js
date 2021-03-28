4f2175eecb21c2d0fe141c1f254222a922b7b81a
Abu Taher <abut70243@gmail.com>
2021-03-28 17:59:35 +0600

    heroku msg

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Heroku')
 })
 
 client.connect(err => {
